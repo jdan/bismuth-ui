eb3ce67cabfed33fe63aba10cb1e3bd6082a2e7d
Jordan Scales <scalesjordan@gmail.com>
2019-04-01 19:47:15 -0400

    start to add node components and eval

--- src/App.js
+++ src/App.js
@@ -1,10 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const Across = ({ children }) => (
+  <div style={{ display: "flex" }}>{children}</div>
+);
+const Down = ({ children }) => (
+  <div style={{ display: "flex", flexDirection: "column" }}>{children}</div>
+);
+
+const Application = ({ op, args, onChange }) => (
+  <Across>
+    {render(op, newOp => onChange({ op: newOp, args }))}
+    <Down>
+      {args.map((arg, i) =>
+        render(arg, newArg =>
+          onChange({
+            op,
+            args: [...args.slice(0, i), newArg, ...args.slice(i + 1)]
+          })
+        )
+      )}
+    </Down>
+  </Across>
+);
+
+const Number = ({ value, onChange }) => (
+  <input
+    onChange={e =>
+      onChange({
+        type: "Number",
+        value: parseInt(e.target.value || 0)
+      })
+    }
+    value={value}
+  />
+);
+
+const Variable = ({ name, onChange }) => (
+  <input
+    onChange={e =>
+      onChange({
+        type: "Variable",
+        name: e.target.value
+      })
+    }
+    value={name}
+  />
+);
+
+function render(node, onChange) {
+  switch (node.type) {
+    case "Application":
+      return <Application {...node} onChange={onChange} />;
+    case "Number":
+      return <Number {...node} onChange={onChange} />;
+    case "Variable":
+      return <Variable {...node} onChange={onChange} />;
+    default:
+      return null;
+  }
+}
 
 export default () => {
-  const [count, setCount] = useState(0);
-  return <>
-    {count}
-    <button onClick={() => setCount(count + 1)}>+</button>
-    <button onClick={() => setCount(count - 1)}>-</button>
-  </>
+  const [ast, setAst] = useState({ type: "Number", value: 6 });
+
+  return (
+    <div>
+      {render(ast, setAst)}
+      <div>Result: {JSON.stringify(evalAst(ast, []))}</div>
+    </div>
+  );
 };
+
+function evalAst(ast, env) {
+  switch (ast.type) {
+    case "Application":
+      return evalAst(ast.caller, env).apply(
+        null,
+        ast.args.map(arg => evalAst(arg, env))
+      );
+    case "Number":
+      return ast.value;
+    case "Variable":
+      return env[ast.name] || null;
+    default:
+      return null;
+  }
+}
